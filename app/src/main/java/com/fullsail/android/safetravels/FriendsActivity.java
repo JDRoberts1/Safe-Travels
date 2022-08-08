f185cbe1818faf6c0c769da3a2d30f4dc53ab5c6
Jeanai Roberts <jdroberts1@student.fullsail.edu>
2022-08-07 19:21:26 -0500

    Initial commit

---
+++ app/src/main/java/com/fullsail/android/safetravels/FriendsActivity.java
@@ -0,0 +1,65 @@
+package com.fullsail.android.safetravels;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    BottomNavigationView navView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+        setUpBottomNav();
+    }
+
+    public void setUpBottomNav(){
+        navView = findViewById(R.id.nav_view);
+        navView.setSelectedItemId(R.id.navigation_home);
+
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Check if selected item is the page user is already on
+                if (item.getItemId() != navView.getSelectedItemId()){
+
+                    // Check selected item id and start activity intent
+                    if (item.getItemId() == R.id.navigation_home){
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(i);
+                    }
+                    else if (item.getItemId() == R.id.navigation_messages){
+                        Intent i = new Intent(getApplicationContext(), MessagesActivity.class);
+                        startActivity(i);
+                    }
+                    else if (item.getItemId() == R.id.navigation_new_post){
+                        Intent i = new Intent(getApplicationContext(), NewPostActivity.class);
+                        startActivity(i);
+                    }
+                    else if (item.getItemId() == R.id.navigation_friends){
+                        Intent i = new Intent(getApplicationContext(), FriendsActivity.class);
+                        startActivity(i);
+                    }
+                    else if (item.getItemId() == R.id.navigation_profile){
+                        Intent i = new Intent(getApplicationContext(), ProfileActivity.class);
+                        startActivity(i);
+                    }
+                    overridePendingTransition(0,0);
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/fullsail/android/safetravels/NewPostActivity.java
@@ -0,0 +1,14 @@
+package com.fullsail.android.safetravels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+    }
+}
