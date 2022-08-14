80719f6b1e3eba15041855a3ba129683067dc1ca
Jeanai Roberts <jdroberts1@student.fullsail.edu>
2022-08-14 11:58:02 -0500

    Update ConversationListActivity.java

--- app/src/main/java/com/fullsail/android/safetravels/ConversationListActivity.java
+++ app/src/main/java/com/fullsail/android/safetravels/ConversationListActivity.java
@@ -1,17 +1,14 @@
 package com.fullsail.android.safetravels;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class ConversationListActivity extends AppCompatActivity {
 
@@ -30,12 +27,9 @@ public class ConversationListActivity extends AppCompatActivity {
     }
 
     // TODO: Set up OnClick for new Message
-    View.OnClickListener newMessageClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            // Take the user to the user search activity
-            startActivity(new Intent(ConversationListActivity.this, UserMessageSearchActivity.class));
-        }
+    View.OnClickListener newMessageClick = v -> {
+        // Take the user to the user search activity
+        startActivity(new Intent(ConversationListActivity.this, UserMessageSearchActivity.class));
     };
 
     // TODO: Set up RCV for users messages
@@ -48,35 +42,32 @@ public class ConversationListActivity extends AppCompatActivity {
         navView = findViewById(R.id.navView);
         navView.setSelectedItemId(R.id.navigation_messages);
 
-        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                // Check if selected item is the page user is already on
-                if (item.getItemId() != navView.getSelectedItemId()){
+        navView.setOnItemSelectedListener(item -> {
+            // Check if selected item is the page user is already on
+            if (item.getItemId() != navView.getSelectedItemId()){
 
-                    // Check selected item id and start activity intent
-                    if (item.getItemId() == R.id.navigation_home){
-                        Intent i = new Intent(getApplicationContext(), HomeActivity.class);
-                        startActivity(i);
-                    }
-                    else if (item.getItemId() == R.id.navigation_new_post){
-                        Intent i = new Intent(getApplicationContext(), NewPostActivity.class);
-                        startActivity(i);
-                    }
-                    else if (item.getItemId() == R.id.navigation_friends){
-                        Intent i = new Intent(getApplicationContext(), FriendsActivity.class);
-                        startActivity(i);
-                    }
-                    else if (item.getItemId() == R.id.navigation_profile){
-                        Intent i = new Intent(getApplicationContext(), UserProfileActivity.class);
-                        startActivity(i);
-                    }
-                    overridePendingTransition(0,0);
-                    return true;
+                // Check selected item id and start activity intent
+                if (item.getItemId() == R.id.navigation_home){
+                    Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(i);
                 }
-
-                return false;
+                else if (item.getItemId() == R.id.navigation_new_post){
+                    Intent i = new Intent(getApplicationContext(), NewPostActivity.class);
+                    startActivity(i);
+                }
+                else if (item.getItemId() == R.id.navigation_friends){
+                    Intent i = new Intent(getApplicationContext(), FriendsActivity.class);
+                    startActivity(i);
+                }
+                else if (item.getItemId() == R.id.navigation_profile){
+                    Intent i = new Intent(getApplicationContext(), UserProfileActivity.class);
+                    startActivity(i);
+                }
+                overridePendingTransition(0,0);
+                return true;
             }
+
+            return false;
         });
 
     }
